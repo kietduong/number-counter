1f15c1f3c7b23c41754f5a1f31aaf04484df2891
Kiet Duong and Ryan Loos <kiet@>
2016-08-16 13:52:08 -0700

    create word puzzle in two ways

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
 // //<!-- Back End -->
 var output = [];
+var vowels = ['a','e','i','o','u'];
 
 function countUp(multiple, end) {
   for (i = 0; i <= end; i += multiple) {
     output.push(i);
   }
 }
+
+// function removeVowels(string) {
+//   var vowels = ['a','e','i','o','u'];
+//   var finalArray = [];
+//   var newArray = string.split('');
+//   for (i = 0; i <= newArray.length; i++) {
+//     if (vowels.includes(newArray[i])) {
+//       finalArray.push('-');
+//     } else {
+//       finalArray.push(newArray[i]);
+//     }
+//   }
+//   return finalArray.join("");
+// };
+
+function removeVowels(string) {
+  var vowels = ['a','e','i','o','u'];
+  var newArray = string.split('');
+  for (i = 0; i <= newArray.length; i++) {
+    if (vowels.includes(newArray[i])) {
+      newArray.splice(i,1,"-");
+    }
+  }
+  return newArray.join("");
+};
+
+
 
 //<!-- Front End  -->
 $(document).ready(function() {
@@ -17,12 +45,19 @@ $(document).ready(function() {
     } else if (countTo < 0 || countBy < 0) {
       alert('use positive numbers you grumpy muthatrucka');
     } else if (countTo < countBy) {
-      alert('don\'t be a jackass');
+      alert('don\t be a jackass');
     } else {
     countUp(countBy, countTo);
     $('#results').append(output.toString());
     //prevent countby from being higher than countTo
     event.preventDefault();
+    // this is the end of the numbers thing dawg, now we start the words thing.
     }
+  });
+  $('#wordForm').submit(function(event) {
+    var sentence = $('#sentenceInput').val();
+    var newSentence = removeVowels(sentence);
+    $('#results2').append(newSentence);
+    event.preventDefault();
   });
 });
